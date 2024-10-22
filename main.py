eb9bb45c240bf0b83d3715da4d2a1cddc29e1041
Dhrumil Bhut <85599841+dhrumilbhut@users.noreply.github.com>
2024-10-22 16:51:11 +0530

    updated

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from tensorflow.keras.models import load_model
 
 # Load the IMDB word index
 word_index = imdb.get_word_index()
-reverse_word_index = {value: key for key, value in word_index.items()}
 
 # Load the pre-trained model
 model = load_model('simple_rnn_imdb.h5')
@@ -16,16 +15,14 @@ model = load_model('simple_rnn_imdb.h5')
 # Encode the review
 def preprocess_text(review):
     words = review.lower().split()
-    encoded_review = [word_index.get(word, 2) + 3 for word in words]
-    padded_review = sequence.pad_sequences([encoded_review], maxlen=500)
+    encoded_review = [word_index.get(word, 2) + 3 for word in words if word_index.get(word, 2) < 10000]
+    padded_review = sequence.pad_sequences([encoded_review], maxlen=700)
     return padded_review
 
 # Prediction function
 def predict_sentiment(review):
     preprocessed_review = preprocess_text(review)
-    print('--------------------------------------------------------------------')
     prediction = model.predict(preprocessed_review)
-    print(prediction)
     sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
     return sentiment, prediction[0][0]
 
@@ -42,7 +39,6 @@ user_input = st.text_area('Enter your review here:')
 if st.button('Classify'):
     sentiment, score = predict_sentiment(user_input)
     st.write(f'Sentiment: {sentiment}')
-    st.write(f'Review: {user_input}')
-    st.write(f'Prediction Score: {score}')
+    st.write(f'Prediction Score: {score:.2f}')
 else:
     st.write('Please enter a movie review to predict the sentiment.')
